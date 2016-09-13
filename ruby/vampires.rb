44b1a3edcf39a1a8134f6cce7b341a7b4c80e94b
emgreen33 <greenemily33@gmail.com>
2016-09-13 15:57:24 -0500

    include Date class and separate names

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,16 +1,22 @@
-#Release 0
+require 'date'
 
 puts "How many employees will be processed?"
 employees = gets.chomp.to_i
 i = 1
 until i > employees
-    result = ""
+    result = nil
     puts "What is your name?"
     name = gets.chomp
+    if name == "Drake Cula" || name == "Tu Fang"
+        result = "Definitely a vampire."
+    end
     puts "How old are you?"
     age = gets.chomp.to_i
     puts "What year were you born?"
     year = gets.chomp.to_i
+    # if (Date.now - year) - age != 0
+    #     liar = true
+    # end
     puts "Our company cafeteria serves garlic bread. Should we order some for you?"
     order = gets.chomp
     puts "Would you like to enroll in the company’s health insurance?"
@@ -21,21 +27,21 @@ until i > employees
         if allergies == "done"
             break
         elsif allergies == "sunshine"
-            result = "Probably a vampire."
+            result = "Probably a vampire." unless result != nil
             break
         end
     end
-    unless result == "Probably a vampire"
-        if (age == (2016 - year)) && (order == "yes" || insurance == "yes") && !(name == "Drake Cula" || name == "Tu Fang")
+    unless result != nil
+        if (age == (2016 - year)) && (order == "yes" || insurance == "yes")
+         # if liar && (order == "yes" || insurance == "yes")
             result = "Probably not a vampire."
         elsif (age != (2016 - year))
+         # elsif !liar
             if (order == "no" || insurance == "no") && !(order == "no" && insurance == "no")
                 result = "Probably a vampire."
             elsif order == "no" && insurance == "no"
                 result = "Almost certainly a vampire."
             end
-        elsif name == "Drake Cula" || name == "Tu Fang"
-            result = "Definitely a vampire."
         else
             result = "Results inconclusive."
         end
@@ -45,5 +51,3 @@ until i > employees
 end
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
-
-#Release 1,2,3,4,5

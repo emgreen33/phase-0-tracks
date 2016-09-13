29fbce81dfc759243167d6530f368fbac3e9da96
emgreen33 <greenemily33@gmail.com>
2016-09-13 14:31:00 -0500

    vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,18 +2,9 @@
 
 puts "How many employees will be processed?"
 employees = gets.chomp.to_i
-
 i = 1
 until i > employees
-    puts "Name any allergies you have one at a time"
-    while true
-        allergies = gets.chomp
-        if allergies == "done"
-            break
-        else
-            true
-        end
-    end
+    result = ""
     puts "What is your name?"
     name = gets.chomp
     puts "How old are you?"
@@ -24,21 +15,32 @@ until i > employees
     order = gets.chomp
     puts "Would you like to enroll in the company’s health insurance?"
     insurance = gets.chomp
-    result = ""
-    if (age == (2016 - year)) && (order == "yes" || insurance == "yes") && !(name == "Drake Cula" || name == "Tu Fang")
-        result = "Probably not a vampire."
-    elsif (age != (2016 - year))
-        if (order == "no" || insurance == "no") && !(order == "no" && insurance == "no")
+    puts "Name any allergies you have one at a time"
+    while true
+        allergies = gets.chomp
+        if allergies == "done"
+            break
+        elsif allergies == "sunshine"
             result = "Probably a vampire."
-        elsif order == "no" && insurance == "no"
-            result = "Almost certainly a vampire."
+            break
         end
-    elsif name == "Drake Cula" || name == "Tu Fang"
-        result = "Definitely a vampire."
-    else
-        result = "Results inconclusive."
     end
-    p result
+    unless result == "Probably a vampire"
+        if (age == (2016 - year)) && (order == "yes" || insurance == "yes") && !(name == "Drake Cula" || name == "Tu Fang")
+            result = "Probably not a vampire."
+        elsif (age != (2016 - year))
+            if (order == "no" || insurance == "no") && !(order == "no" && insurance == "no")
+                result = "Probably a vampire."
+            elsif order == "no" && insurance == "no"
+                result = "Almost certainly a vampire."
+            end
+        elsif name == "Drake Cula" || name == "Tu Fang"
+            result = "Definitely a vampire."
+        else
+            result = "Results inconclusive."
+        end
+    end
+    puts result
     i += 1
 end
-
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."

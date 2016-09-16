918cfc5ff5db2e7d9917f67b1735a05314b1d5cb
emgreen33 <greenemily33@gmail.com>
2016-09-16 13:41:25 -0500

    release 3 complete for edge cases

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -9,13 +9,16 @@
 def encrypt(string)
     index = 0
     while index < string.length
-        string[index] = string[index].next
+        if string[index] == "z"
+            string[index] = "a"
+        else
+            string[index] = string[index].next
+        end
         index += 1
     end
     string
 end
 
-p encrypt("hello")
 
 # Decrypt
 # 1. Define a decrypt method which takes a string argument as its parameter
@@ -29,3 +32,14 @@ p encrypt("hello")
 def decrypt(string)
     index = 0
     alphabet = "abcdefghijklmnopqrstuvwxyz"
+    while index < string.length
+        string[index] = alphabet[alphabet.index(string[index])-1]
+        index += 1
+    end
+    string
+end
+
+p encrypt("abc")
+p encrypt("zed")
+p decrypt("bcd")
+p decrypt("afe")

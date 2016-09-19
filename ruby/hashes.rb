10b2cff7f620df620c6c1e4e623a22ff5d1dfe1d
emgreen33 <greenemily33@gmail.com>
2016-09-19 16:11:40 -0500

    write pseudocode for design program

---
+++ ruby/hashes.rb
@@ -0,0 +1,12 @@
+Pseudocode:
+- Ask user for their name, age, no. of children, decor theme, if they like open spaces, vintage or modern, pets, no. of bedrooms, no. of bathrooms, budget
+- save their answers as key: value pairs in a hash and convert to approp data type
+- print the hash when all questions answered
+- ask user if they want to update any key? ask for the key
+    - if yes,
+            - ask the user for the new value and update the :key
+    - elsif no, end if they say "none"
+- print latest version of hash and exit
+
+
+

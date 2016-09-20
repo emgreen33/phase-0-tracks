1cd4a454184647a2df1c51b75ac2454829a3fa17
emgreen33 <greenemily33@gmail.com>
2016-09-19 22:25:04 -0500

    design and build nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,45 @@
+student = {
+    teddy: {
+        english: "A",
+        math: "C",
+        french: "A",
+        science: {
+            chemistry: 95,
+            physics: 76
+        },
+        sport: "B",
+        extra_curric: [
+            "Debating Team",
+            "Chess Club"
+        ]
+    },
+
+    toby: {
+        english: "B",
+        math: "B",
+        french: "D",
+        science: {
+            chemistry: 70,
+            physics: 59
+        },
+        sport: "D",
+        extra_curric: [
+            "Football Team"
+        ]
+    },
+
+    dory: {
+        english: "N/A",
+        math: "B",
+        french: "D",
+        science: {
+            chemistry: 80,
+            physics: 82
+        },
+        sport: "A",
+        extra_curric: [
+            "Tennis Team",
+            "Glee Club"
+        ]
+    },
+}

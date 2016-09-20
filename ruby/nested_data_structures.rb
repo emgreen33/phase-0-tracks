33f201eb6ee0a6e28051e6f6f45d3cdb51dd48fb
emgreen33 <greenemily33@gmail.com>
2016-09-19 22:29:02 -0500

    print few individual pieces of data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,4 +1,4 @@
-student = {
+students = {
     teddy: {
         english: "A",
         math: "C",
@@ -43,3 +43,9 @@ student = {
         ]
     },
 }
+
+# Once you've built it, print a few individual pieces of deeply nested data from the structure, showing that you know how to use multiple indexes or hash keys (or both) to access nested items. Try to demonstrate a few different types of access.
+
+p students[:dory][:extra_curric][0]
+p students[:toby][:english]
+p students[:teddy][:science][:chemistry]

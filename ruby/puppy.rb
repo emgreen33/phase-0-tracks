57525f66000c8077527ab5b60390af0d60d58e46
emgreen33 <greenemily33@gmail.com>
2016-09-26 14:23:43 -0500

    Design a puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+#SPECIES --------------------------------
+#Basenji Puppy
+
+#CHARACTERISTICS ------------------
+# Friendly: yes
+# Atheltic: yes
+# Color: tri/brown
+# Diet: varies
+# Trainable: yes
+# Loud: no
+
+# BEHAVIOR --------------------------------
+# Eat
+# Pee
+# Run
+# Jump
+# Yodle

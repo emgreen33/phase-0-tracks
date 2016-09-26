d2cafcebffedc0a77e2ed3439848debdcb088918
emgreen33 <greenemily33@gmail.com>
2016-09-26 18:14:11 -0500

    build program that creates lots of santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,8 @@
+#Release 0, 1, 2, 3
+
 class Santa
     attr_reader :ethnicity, :age
-    attr_accessor :gender
+    attr_accessor :gender, :age
 
     def initialize(gender, ethnicity)
         puts "Initializing Santa instance..."
@@ -42,20 +44,25 @@ end
 #     santas << Santa.new(genders[i], ethnicities[i])
 # end
 
-santa= Santa.new("female", "white")
-santa.about
-santa.gender = "male"
-santa.about
-santa.get_mad_at("Dasher")
-santa.about
+# santa= Santa.new("female", "white")
+# santa.about
+# santa.gender = "male"
+# santa.about
+# santa.get_mad_at("Dasher")
+# santa.about
 
-#Should return:
-# Initializing Santa instance...
-# Gender: female
-# Ethnicity: white
-# Reindeer ranking: ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-# Age: 0
-# Gender: male
-# Ethnicity: white
-# Reindeer ranking: ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-# Age: 0
+#Release 4: Build Many, Many Santas
+# - declare two arrays of example genders and example ethnicities
+# - use a while loop to generate a large number of santas
+# - assign random genders and ethnicities to each santa and set age to random number between 0 and 140
+# - print out the attributes of each Santa (using about)
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "N/A"]
+index = 0
+while index < 10
+    santas = Santa.new(example_genders.sample, example_ethnicities.sample)
+    santas.age = rand(0..140)
+    santas.about
+    index += 1
+end

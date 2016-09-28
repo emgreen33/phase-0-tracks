35ad655e4dfe10d10ade7bb7d55a73cd0cd37abe
emgreen33 <greenemily33@gmail.com>
2016-09-28 17:11:43 -0500

    add driver code and puppy methods for release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,34 @@
+#Release 0
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+    def fetch(toy)
+        puts "I brought back the #{toy}!"
+        toy
+    end
+
+    def speak(num)
+        num.times { | i | puts "Woof!"}
+    end
+
+    def roll_over
+        puts "*rolls over*"
+    end
+
+    def dog_years(years)
+        dog_years = years * 7
+        puts "Your dog is #{dog_years} years old"
+        dog_years
+    end
+
+    def sit
+        puts "*sits down*"
+    end
 
 end
 
+toby = Puppy.new
+p toby.fetch("ball")
+toby.speak(3)
+toby.roll_over
+toby.dog_years(4)
+toby.sit

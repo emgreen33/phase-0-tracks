48c4d49c674a44dae2ff41efe2f2e4bd11a36689
emgreen33 <greenemily33@gmail.com>
2016-09-28 17:29:59 -0500

    write own bartender class and store instances in array

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,8 @@
 #Release 0
 class Puppy
+    def initialize
+        puts "Initializing new puppy instance ..."
+    end
 
     def fetch(toy)
         puts "I brought back the #{toy}!"
@@ -32,3 +35,35 @@ toby.speak(3)
 toby.roll_over
 toby.dog_years(4)
 toby.sit
+
+# Release 2
+
+class Bartender
+
+    def initialize
+        puts "Creating a new bartender..."
+    end
+
+    def check_id(age)
+        if age >= 21
+            puts "Hello, what can I get you?"
+        else
+            puts "GET OUT."
+        end
+    end
+
+    def shake(cocktail)
+        puts "*shakes #{cocktail}*"
+    end
+
+end
+
+bartender_array = []
+50.times do |i|
+    bartender_array << Bartender.new
+end
+
+bartender_array.each do |bartender|
+    bartender.check_id(rand(0..70))
+    bartender.shake("manhattan")
+end

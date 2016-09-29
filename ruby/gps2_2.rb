0503b92f778f275b856961c4a1a9b8ca2702fa95
emgreen33 <greenemily33@gmail.com>
2016-09-29 18:17:52 -0500

    write pseudocode for list methods

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,28 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # split the string into an array separated by spaces
+  # set default quantity to one and add it into our list
+  # print the list to the console
+# output: print the hash shopping list
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: add the grocery item as a item and quantity pair to shopping list
+# output: print the updated hash shopping list
+
+# Method to remove an item from the list
+# input: item the user wants to remove
+# steps: take item to symbol and check if it's in the list then delete it if it is
+# output: print the new modified shopping list
+
+# Method to update the quantity of an item
+# input: item the user wants to update and quantity
+# steps: check if it's there and then update new quantity by setting the key to new quanityt
+# output: print the new modified shopping list
+
+# Method to print a list and make it look pretty
+# input: takes in the shopping list hash
+# steps: iterate through each key/value pair
+# output: print a clean list to user
+

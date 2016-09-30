2b0770e8983faef0ae7a1d6c035cabb410c93068
emgreen33 <greenemily33@gmail.com>
2016-09-30 17:05:07 -0500

    write code to make RSpect tests pass the spec tests

---
+++ ruby/word_game/game.rb
@@ -0,0 +1,61 @@
+class Hangman
+    attr_reader :word, :guesses_left
+
+    def initialize(word)
+        @word = word
+        @blank_lines = ("_" * word.length).split("")
+        @guesses_left = word.length
+        @word_letters = @word.split("")
+        @guessed_letters = ["a"]
+    end
+
+    def good_letter(letter)
+        if letter =~ /\d/
+            return "Please enter a valid letter."
+        elsif @guessed_letters.include?(letter)
+            return "You already guessed that letter."
+        else
+            @guessed_letters << letter unless @word.count(letter) > 1
+            return true
+        end
+    end
+
+    def check(letter)
+        if good_letter(letter) == true
+            p in_word?(letter)
+        else
+            p good_letter(letter)
+        end
+    end
+
+    def in_word?(letter)
+        if @word.include?(letter)
+            fill_blank(letter)
+            return "Good guess!"
+        else
+            guesses_reduced
+            return "Not in word."
+        end
+    end
+
+    def fill_blank(letter)
+        @word.length.times do | index |
+            if @word_letters[index] == letter
+                @blank_lines[index] = letter
+            end
+        end
+        @blank_lines.join("")
+    end
+
+    def guesses_reduced
+        @guesses_left -= 1
+    end
+
+    def blank_lines
+        @blank_lines.join("")
+    end
+
+    def wins_game?
+        @blank_lines.join("") == @word
+    end
+end

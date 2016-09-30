90f88b6425e839077bc2044005015fa4c78f5865
emgreen33 <greenemily33@gmail.com>
2016-09-30 17:03:00 -0500

    write RSpec test for each feature to be added to the game

--- ruby/word_game/game_spec.rb
+++ ruby/word_game/game_spec.rb
@@ -12,3 +12,45 @@
 #                   - replace all instances of the letter in the blank line word
 # - Check if the player has won the game by comparing the blank word with the given word
 #---------------------
+
+require_relative 'game'
+
+describe Hangman do
+
+    let(:game) { Hangman.new("bater") }
+
+    it "checks if the letter guessed is valid, a number, or already guessed" do
+        expect(game.good_letter("b")).to eq true
+        expect(game.good_letter("9")).to eq "Please enter a valid letter."
+        expect(game.good_letter("b")). to eq "You already guessed that letter."
+    end
+
+    it "checks if a letter is in a word" do
+        expect(game.check("b")).to eq "Good guess!"
+        expect(game.check("c")).to eq "Not in word."
+        expect(game.check("9")).to eq "Please enter a valid letter."
+    end
+
+    it "checks if a letter is in a word" do
+        expect(game.in_word?("m")).to eq "Not in word."
+        expect(game.in_word?("e")).to eq "Good guess!"
+    end
+
+    it "replaces a blank line with the correct letter" do
+        expect(game.fill_blank("x")).to eq "_____"
+        expect(game.fill_blank("b")).to eq "b____"
+    end
+
+    it "checks that game is reduced by 1" do
+        expect(game.guesses_reduced).to eq 4
+    end
+
+    it "checks that blank line array joins together" do
+        expect(game.blank_lines).to eq "_____"
+    end
+
+    it "checks if a player has won the game" do
+        expect(game.wins_game?).to eq false
+    end
+
+end

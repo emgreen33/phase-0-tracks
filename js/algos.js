38eb5902cb22f3031052b348b7f0a2e9424ac688
emgreen33 <greenemily33@gmail.com>
2016-10-03 17:01:28 -0500

    refactor pseudocode and write function for find key value match

--- js/algos.js
+++ js/algos.js
@@ -14,20 +14,33 @@ function longestPhrase(array) {
             longest = array[i];
         };
     } return longest;
-}
+};
 
 
 //Release 1: Find a Key-Value Match
 // Declare a function hasMatch that will take in two objects as parameters
 // Declare a variable match, and set it to false
+// Create an array of the keys for object 1
 // Set index at 0, until the index is equal to the length of object 1, iterate through
-        // For each object at index i,
-                // If the key value pair matches
-                        // set variable match, to true
+        // For each object, check if the value of the same key is equal
+                // If the values match
+                    // return true
+        // Otherwise return false
 // Return the variable match
 
+function hasMatch(object1, object2) {
+    keys1 = Object.keys(object1);
+    for (var i = 0; i < keys1.length; i++ ) {
+        if (object1[keys1[i]] == object2[keys1[i]]) {
+            return true;
+        };
+    }; return false;
+};
 
+var personOne = {name: "Bob", age: 30, city: "Chicago"};
+var personTwo = {name: "Emily", age: 25, city: "Chicago"};
 
+console.log(hasMatch(personOne, personTwo));
 
 
 
@@ -41,11 +54,11 @@ function longestPhrase(array) {
 
 
 //Driver Code:
-var sample = ["long phrase","longest phrase","longer phrase"];
-console.log(longestPhrase(sample));
+// var sample = ["long phrase","longest phrase","longer phrase"];
+// console.log(longestPhrase(sample));
 
-var anotherSample = ["shortest", "second longest", "the most long of all", "wrong"];
-console.log(longestPhrase(anotherSample));
+// var anotherSample = ["shortest", "second longest", "the most long of all", "wrong"];
+// console.log(longestPhrase(anotherSample));
 
-var finalSample = ["this is", "another test", "to make sure", "my longest phrase function works"];
-console.log(longestPhrase(finalSample));
+// var finalSample = ["this is", "another test", "to make sure", "my longest phrase function works"];
+// console.log(longestPhrase(finalSample));

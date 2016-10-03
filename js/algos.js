14954d0fd2c2a58e63b253f7e714289b0cf63e1b
emgreen33 <greenemily33@gmail.com>
2016-10-03 18:44:53 -0500

    write pseudocode for release 2 random test data

--- js/algos.js
+++ js/algos.js
@@ -16,10 +16,9 @@ function longestPhrase(array) {
     } return longest;
 };
 
-
+//---------------------------------------------------------------------------------------------------------------------
 //Release 1: Find a Key-Value Match
 // Declare a function hasMatch that will take in two objects as parameters
-// Declare a variable match, and set it to false
 // Create an array of the keys for object 1
 // Set index at 0, until the index is equal to the length of object 1, iterate through
         // For each object, check if the value of the same key is equal
@@ -29,36 +28,48 @@ function longestPhrase(array) {
 // Return the variable match
 
 function hasMatch(object1, object2) {
-    keys1 = Object.keys(object1);
+    var keys1 = Object.keys(object1);
     for (var i = 0; i < keys1.length; i++ ) {
-        if (object1[keys1[i]] == object2[keys1[i]]) {
+        if (object1[keys1[i]] === object2[keys1[i]]) {
             return true;
         };
     }; return false;
 };
 
-var personOne = {name: "Bob", age: 30, city: "Chicago"};
-var personTwo = {name: "Emily", age: 25, city: "Chicago"};
+//---------------------------------------------------------------------------------------------------------------------
+// Release 2: Generate Random Test Data
+// Declare a function that takes an integer as a parameter
+// Create an array of letters from a to z
+// Create a variable of an empty array
+// Create a variable of empty word string
+        // While i is less than the number given as argument,
+                // generate random number between 1 and 10 for number of letters in word
+                // While j is less than the random number of word length
+                        // Generate a random number between 0 and 25 for alphabet index
+                        // Add the letter into the word string
+        // Push the random word into the array
+        // Reset the word string to empty string
+// Return the new random array
 
-console.log(hasMatch(personOne, personTwo));
+
+// Add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word" function, and prints the result.
 
 
-
-
-
-
-
-
-
-
-
-
+//---------------------------------------------------------------------------------------------------------------------
 //Driver Code:
+
+//Release 0
 // var sample = ["long phrase","longest phrase","longer phrase"];
 // console.log(longestPhrase(sample));
-
 // var anotherSample = ["shortest", "second longest", "the most long of all", "wrong"];
 // console.log(longestPhrase(anotherSample));
-
 // var finalSample = ["this is", "another test", "to make sure", "my longest phrase function works"];
 // console.log(longestPhrase(finalSample));
+
+
+// //Release 1
+// var personOne = {name: "Bob", age: 30, city: "Chicago"};
+// var personTwo = {name: "Emily", age: 25, city: "Chicago"};
+// console.log(hasMatch(personOne, personTwo));
+
+

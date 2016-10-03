0ae41fd30ceaba1525689f522238bb7dff11d429
emgreen33 <greenemily33@gmail.com>
2016-10-03 14:03:14 -0500

    create exploer file and add pseudocode for reverse function

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// Release 4: Write a reverse function
+// Declare a function that takes in a string as a parameter
+// Declare an empty string variable reversedString
+// Create the for loop, where the index is set at the last letter
+//      - as long as the index is greater or equal to 0 (the first letter) it will run
+//      - for each letter in the string, it will be added to reversedString starting with the last
+// return the reversedString
+

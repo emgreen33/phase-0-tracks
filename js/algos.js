ff6787cafcfb7558dc314af920e77e2a33773152
emgreen33 <greenemily33@gmail.com>
2016-10-03 15:44:55 -0500

    write pseudocode for release 1 find key/value match

--- js/algos.js
+++ js/algos.js
@@ -1,14 +1,11 @@
 //Release 0: Find the longest phrase
-
-//Pseudocode:
 // Declare a function longestPhrase that will take in an array of strings
 // Declare a new variable, longest, that is set initially at null and will be updated
-// Set index at 0, until the index is equal to the length of the array-1 iterate through
+// Set index at 0, until the index is equal to the length of the array iterate through
             // For each phrase in the array,
                     // If the next phrase is longer than the longest variable,
                             // Update the longest variable
 // Return the longest phrase variable
-
 
 function longestPhrase(array) {
     longest = array[0];
@@ -19,6 +16,31 @@ function longestPhrase(array) {
     } return longest;
 }
 
+
+//Release 1: Find a Key-Value Match
+// Declare a function hasMatch that will take in two objects as parameters
+// Declare a variable match, and set it to false
+// Set index at 0, until the index is equal to the length of object 1, iterate through
+        // For each object at index i,
+                // If the key value pair matches
+                        // set variable match, to true
+// Return the variable match
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Driver Code:
 var sample = ["long phrase","longest phrase","longer phrase"];
 console.log(longestPhrase(sample));
 

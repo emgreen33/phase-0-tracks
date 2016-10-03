7aae849b0e6fc437ff814a8a84b26bfac0e09577
emgreen33 <greenemily33@gmail.com>
2016-10-03 14:06:20 -0500

    implement javascript return function and test with driver code

--- js/explore.js
+++ js/explore.js
@@ -1,8 +1,21 @@
 // Release 4: Write a reverse function
 // Declare a function that takes in a string as a parameter
 // Declare an empty string variable reversedString
-// Create the for loop, where the index is set at the last letter
-//      - as long as the index is greater or equal to 0 (the first letter) it will run
-//      - for each letter in the string, it will be added to reversedString starting with the last
-// return the reversedString
+// Create a for loop,
+//      - Set the index to the value of the last letter
+//      - While the index is greater or equal to 0 (first letter), run the loop
+//      - Take 1 from index each time
+//      - For each letter in the string, add it to reversedString starting with the last
+// Return the reversedString
+
+function reverse(string) {
+    var reversedString = "";
+    for (var i = string.length-1; i >= 0; i-- ) {
+        reversedString += string[i];
+    }
+    return reversedString;
+}
+
+// Call the function using Javascripts call syntax:
+console.log(reverse("hello"));
 

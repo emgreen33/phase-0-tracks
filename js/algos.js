5a356ecec322e6bf27b092b19d58d2ae97c3a406
emgreen33 <greenemily33@gmail.com>
2016-10-03 15:39:39 -0500

    write function for longest phrase and driver code to check

--- js/algos.js
+++ js/algos.js
@@ -1,8 +1,4 @@
 //Release 0: Find the longest phrase
-// Write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
-//So if we gave your function the array of ["long phrase","longest phrase","longer phrase"], it would return "longest phrase".
-//This is a logical reasoning challenge. Note that "some string".length will do what you expect in JS.
-// Add driver code that tests your function on a few arrays.
 
 //Pseudocode:
 // Declare a function longestPhrase that will take in an array of strings
@@ -11,8 +7,23 @@
             // For each phrase in the array,
                     // If the next phrase is longer than the longest variable,
                             // Update the longest variable
-                     // Else leave it unchanged
 // Return the longest phrase variable
 
 
+function longestPhrase(array) {
+    longest = array[0];
+    for (var i = 0; i < array.length; i++ ) {
+        if (array[i].length > longest.length) {
+            longest = array[i];
+        };
+    } return longest;
+}
 
+var sample = ["long phrase","longest phrase","longer phrase"];
+console.log(longestPhrase(sample));
+
+var anotherSample = ["shortest", "second longest", "the most long of all", "wrong"];
+console.log(longestPhrase(anotherSample));
+
+var finalSample = ["this is", "another test", "to make sure", "my longest phrase function works"];
+console.log(longestPhrase(finalSample));

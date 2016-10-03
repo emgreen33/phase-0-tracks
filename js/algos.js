1466613ab8fd4454a940686ee8a995f024593973
emgreen33 <greenemily33@gmail.com>
2016-10-03 15:30:52 -0500

    write pseudocode for longest phrase function

---
+++ js/algos.js
@@ -0,0 +1,18 @@
+//Release 0: Find the longest phrase
+// Write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
+//So if we gave your function the array of ["long phrase","longest phrase","longer phrase"], it would return "longest phrase".
+//This is a logical reasoning challenge. Note that "some string".length will do what you expect in JS.
+// Add driver code that tests your function on a few arrays.
+
+//Pseudocode:
+// Declare a function longestPhrase that will take in an array of strings
+// Declare a new variable, longest, that is set initially at null and will be updated
+// Set index at 0, until the index is equal to the length of the array-1 iterate through
+            // For each phrase in the array,
+                    // If the next phrase is longer than the longest variable,
+                            // Update the longest variable
+                     // Else leave it unchanged
+// Return the longest phrase variable
+
+
+

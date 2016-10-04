593f6ddd69de62d7e6a27832dceacff5f2e388d3
emgreen33 <greenemily33@gmail.com>
2016-10-04 17:42:09 -0500

    Complete Release 0

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ['red', 'green', 'blue', 'yellow'];
+var names = ['Matthew', 'Mark', 'Luke', 'John'];
+
+colors.push('purple');
+names.push('Judas');
+
+console.log(colors);
+console.log(names);

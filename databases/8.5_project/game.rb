1c28ec2c047ba9f405a05b1c90867808d29115e8
emgreen33 <greenemily33@gmail.com>
2016-10-13 12:23:49 -0500

    add comments for each method

--- databases/8.5_project/game.rb
+++ databases/8.5_project/game.rb
@@ -34,16 +34,18 @@ class Person
 
 end
 
+#method to create players
 def create_person(db, name, age)
     db.execute("INSERT INTO game(name, age) VALUES (?,?)", [name, age]
         )
 end
 
-# Make some players
+# Make some players: comment out
 # 10.times do
 #     create_person(db, Faker::Name.name, rand(10..20))
 # end
 
+#make instances of players
 table_data = db.execute("SELECT name, age FROM game")
 
 people = []
@@ -52,6 +54,7 @@ table_data.each do | name, age |
     people << Person.new(name, age)
 end
 
+#print out each player
 people.each do | person |
     puts person.print
 end
@@ -59,26 +62,30 @@ end
 puts "=================="
 puts "UPDATED LIST:"
 puts "=================="
-# Change their age
+
+# Change the players age by random amount
 people.each do | person |
     person.age += rand(1..25)
 end
 
+#print each player with updated age
 people.each do | person |
     puts person.print
 end
 
+#method to update database
 def update(db, name, age)
     query = "UPDATE game SET age =#{age} WHERE name=\"#{name}\""
     db.execute(query)
 end
 
+#update table data
 people.each do |person|
     table_data << person.update_table
 end
 
-# p table_data
 
+#update database with new table data
 table_data.each do | name, age |
     update(db,name,age)
 end

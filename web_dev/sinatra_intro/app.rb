a34b727b42970f7be2b561a8d329db6bca126ecc
emgreen33 <greenemily33@gmail.com>
2016-10-19 12:24:43 -0500

    follow with video and add contact and great job routes

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -7,18 +7,20 @@ db.results_as_hash = true
 
 # write a basic GET route
 # add a query parameter
-# GET /
+# GET / (the home directory is /)
+#localhost.8888/?name=Jen&age=35
 get '/' do
   "#{params[:name]} is #{params[:age]} years old."
 end
 
 # write a GET route with
 # route parameters
+#localhost.888/?about/Jen
 get '/about/:person' do
   person = params[:person]
   "#{person} is a programmer, and #{person} is learning Sinatra."
 end
-
+#localhost.888/?Romeo/loves/Juliet
 get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
@@ -43,4 +45,26 @@ end
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+
+#route params are like arguments to a method
+
+#write a /contact route that displays an address (you can make up the address).
+get '/contact/:address' do
+  address = params[:address]
+  "Email address is #{address}"
+end
+#http://localhost:9393/contact/greenemily33@gmail.com
+
+
+#A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
 end
